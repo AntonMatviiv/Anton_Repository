df9d1ddfe3eaca2a43582e5a056df058e2ff5170
AntonMatviiv <amatviiv2021@itcollege.lviv.ua>
2024-09-23 22:17:54 +0300

    Python

---
+++ 1_lab/my_first_app.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+name = "Anton"
+location = "Lviv"
+
+print(f"{name} start programming at {datetime.now()}. {location} is the best city!") 
+# Програма виводить Anton start programming at 2024-09-23 20:47:53.322078. Lviv is the best city!
